9e09258d3f1d5e51e485b4aea312f6549e3d7a24
Noah Appelbaum <noahappelbaum@gmail.com>
2023-10-30 18:12:41 -0500

    Initial "Apply" Form Draft
    Working text input fields. Next steps are geographic location selection and a submit button to make a POST request to the API.

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    location: "" //TODO: default radio?
+  });
+
+  // function handleChange(evt) {
+  //   const { name, value } = evt.target;
+  //   setFormData(curr => ({
+  //     ...curr,
+  //     [name]: value
+  //   }));
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Rithm Application. So glad. To apply.</Text>
       <StatusBar style="auto" />
+
+      <View>
+
+        <TextInput
+        placeholder="First Name"
+        onChangeText={newText => setFormData(curr => ({...curr, firstName:newText}))}
+        value={formData.firstName}
+        />
+
+        <TextInput
+        placeholder="Last Name"
+        onChangeText={newText => setFormData(curr => ({...curr, lastName:newText}))}
+        value={formData.lastName}
+        />
+
+        <TextInput
+        placeholder="Email Address"
+        onChangeText={newText => setFormData(curr => ({...curr, email:newText}))}
+        value={formData.email}
+        />
+
+      </View>
     </View>
   );
 }
